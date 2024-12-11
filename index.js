00b5e80635ee126a25d62d55c193030e185a0354
Talha Tariq <tariqtalha936@gmail.com>
2024-12-12 01:32:10 +0500

    Add Cors to handle Request from 3000 address

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ const notificationRoute = require('./routes/notificationRoute')
 const searchRoute = require('./routes/searchRoute')
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const cors = require("cors");
 // Middleware
 app.use(express.json());
-
+app.use(
+    cors({
+      origin: "http://localhost:3000", // Frontend URL
+      methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+      credentials: true, // Include credentials (if needed)
+    })
+  );
 // Connect Database
 connectDB();
 app.use('/api/user',userRoute);
@@ -23,6 +29,6 @@ app.use("/uploads", express.static("uploads"));
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
-
+app.use(cors());
 // Listen on PORT
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
